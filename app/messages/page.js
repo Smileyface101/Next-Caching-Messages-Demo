e4b9199adfa546b925bd855d9d55714511c7069a
Smileyface101 <beric.vojislav96.pp@gmail.com>
2024-10-08 03:11:30 +0200

    unstable cache next implementation, tags

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,9 +1,9 @@
 import Messages from '@/components/messages';
 import { getMessages } from '@/lib/messages';
 
-export default function MessagesPage() {
+export default async function MessagesPage() {
 
-  const messages = getMessages();
+  const messages = await getMessages();
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
   }
